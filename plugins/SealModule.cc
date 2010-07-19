2d7587c05da0b7d81ba8a2f0b0a631a961e31aa6
konec <>
2010-07-19 09:26:44 +0000

    adding FilterL1 to SealModules (for Mieszko Mackowski).

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -7,6 +7,9 @@ DEFINE_FWK_MODULE(FilterBX);
 #include "FilterOrbit.h"
 DEFINE_FWK_MODULE(FilterOrbit);
 
+#include "FilterL1.h"
+DEFINE_FWK_MODULE(FilterL1);
+
 #include "Filter_L1_GM.h"
 DEFINE_FWK_MODULE(Filter_L1_GM);
 
