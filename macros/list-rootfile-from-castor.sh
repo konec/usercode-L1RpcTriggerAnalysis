64a57f1d23661a43325be87e591f6874e894d870
rybinska <>
2011-04-27 16:59:53 +0000

    *** empty log message ***

---
+++ macros/list-rootfile-from-castor.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -f
+
+runListFile=runsFromJson.list
+base=/castor/cern.ch/cms
+#castor=/store/express/Run2010A/ExpressPhysics/FEVT/v4/000
+castor=/store/express/Run2010B/ExpressPhysics/FEVT/Express-v2/000
+
+#echo  $base/$castor
+
+for runA in `nsls $base/$castor`
+do
+
+path=$base/$castor/$runA
+#echo $path
+
+  for  runB in `nsls $path`
+    do
+	fullpath=$path/$runB
+	#echo $fullpath
+	
+	run=$runA$runB
+	#echo run  $run
+	
+	# runy z json
+	for listRun in in `more $runListFile  | awk '{print $2}'`
+	do
+
+	
+	if [ $listRun = $run ] 
+	then
+	        #echo run from list $listRun $run
+
+		for file in `nsls $fullpath`
+		do		
+		echo $castor/$runA/$runB/$file
+		done
+	fi		
+	done
+
+    done
+
+done	
+
