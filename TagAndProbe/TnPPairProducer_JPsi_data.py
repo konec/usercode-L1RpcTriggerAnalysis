e5e34e6a59b67c0af9e3d4581c7e27bfe599d708
rybinska <>
2012-03-07 11:06:35 +0000

    *** empty log message ***

--- TagAndProbe/TnPPairProducer_JPsi_data.py
+++ TagAndProbe/TnPPairProducer_JPsi_data.py
@@ -3,19 +3,40 @@ import FWCore.ParameterSet.Config as cms
 import math
 
 process = cms.Process("RPCTagAndProbe")
+### standard includes
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.GeometryExtended_cff')
 process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+### global tag
 process.GlobalTag.globaltag = 'GR_R_44_V12::All'
+
+# get list of input files from a file
+# (255 file limit does not apply)
+#import FWCore.Utilities.FileUtils as FileUtils
+#mylist = FileUtils.loadListFromFile(_LIST_OF_FILES_)
+#process.source = cms.Source("PoolSource",
+#fileNames = cms.untracked.vstring( *mylist )
+#)
+
+# apply lumi section mask from JSON file
+#import PhysicsTools.PythonAnalysis.LumiList as LumiList
+#import FWCore.ParameterSet.Types as CfgTypes
+#myLumis = LumiList.LumiList(filename=_JSON_FILE_).getCMSSWString().split(',')
+#process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+#process.source.lumisToProcess.extend(myLumis)
+
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-	'/store/data/Run2011B/MuOnia/RECO/PromptReco-v1/000/178/101/CEA16BB9-5AF6-E011-B1A7-BCAEC518FF5A.root'
+'/store/data/Run2011B/SingleMu/RECO/PromptReco-v1/000/175/832/10EBFA08-84DB-E011-9D72-003048D2C020.root'    
+#'/store/data/Run2011B/MuOnia/RECO/PromptReco-v1/000/178/101/CEA16BB9-5AF6-E011-B1A7-BCAEC518FF5A.root'
     ),
+    #eventsToProcess = cms.untracked.VEventRange("178101:86867-178101:86868")
 )
+### number of events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 ############################################
 ############# P R E F I L T E R ############
@@ -50,51 +71,45 @@ process.globalMuFilter = cms.EDFilter("TrackCountFilter",
     minNumber = cms.uint32(1)
 )
 ############################################
-##  Prefilter
+## -0- Prefilter
 process.preFilter = cms.Sequence(process.JPsiHLT *
                                  process.noScraping * 
                                  process.primaryVertexFilter +  
                                  process.globalMuFilter)
 ############################################                                 
-##  l1muonsAnyBX - from L1extraParticles
+## -1- l1muonsAnyBX - from L1extraParticles
 from L1Trigger.L1ExtraFromDigis.l1extraParticles_cfi import l1extraParticles
 process.l1muonsAnyBX = l1extraParticles.clone(
 centralBxOnly = cms.bool(True)
 )   
-#maczowanie RPC
-#process.l1extraToRPCValueMap = cms.EDProducer("L1ExtraToRPCValueMapProducer",
-#L1Extra = cms.InputTag("l1muonsAnyBX"),
-#) 
-
 ############################################
-##  patMuonsWithTrigger
+## -2- patMuonsWithTrigger
 process.load("MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff")
 ############################################
-## tagMuons - from patMuonsWithTrigger
+## -3- tagMuons - from patMuonsWithTrigger, Tight muon https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMuonId
 process.tagMuons = cms.EDFilter("PATMuonRefSelector",
   src = cms.InputTag("patMuonsWithTrigger"),
   cut = cms.string('isGlobalMuon && isTrackerMuon'\
                    +'&& abs(dB)<0.2 && globalTrack().normalizedChi2<10'\
                    +'&& globalTrack().hitPattern().numberOfValidTrackerHits>10'\
-                   +'&& globalTrack().hitPattern().numberOfValidMuonHits>0'\
+                   +'&& globalTrack().hitPattern().numberOfValidMuonHits>6'\
+		   +'&& numberOfMatchedStations()>1'\
                    +'&& globalTrack().hitPattern().numberOfValidPixelHits>0'\
-                   +'&& numberOfMatches>1'\
                    +'&& (chargedHadronIso()+neutralHadronIso()+photonIso())<0.1*pt'\
                    +'&& pt > 3 & abs(eta) < 2.1'\
-                   +"&&(!triggerObjectMatchesByPath('HLT_Mu*_v*').empty()||!triggerObjectMatchesByPath('HLT_IsoMu*_v*').empty())")
+                   +"&&!triggerObjectMatchesByCollection('hltL3MuonCandidates').empty()"\
+                   +"&&(triggerObjectMatchesByCollection('hltL3MuonCandidates').at(0).hasFilterLabel('hltMu5Track2JpsiTrackMassFiltered')"\
+                   +"||triggerObjectMatchesByCollection('hltL3MuonCandidates').at(0).hasFilterLabel('hltMu7Track7JpsiTrackMassFiltered'))")
 )
 ############################################
-##  goodTracks - from generalTracks
+## -4- goodTracks - from generalTracks
 process.goodTracks = cms.EDFilter("TrackSelector",
     src = cms.InputTag("generalTracks"),
-    cut = cms.string("pt > 3 && abs(eta) < 2.1"),
+    cut = cms.string("pt > 3 && abs(eta) < 2.1&&numberOfValidHits > 11 && hitPattern.pixelLayersWithMeasurement > 1 && normalizedChi2 < 4 && abs(d0) < 3 && abs(dz) < 30"),
 )
 ############################################
-## selectedLayer1TrackCands - from goodTracks
+## -5- selectedLayer1TrackCands - from goodTracks
 process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
-process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi")
-process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi")
-process.load("RecoMuon.DetLayers.muonDetLayerGeometry_cfi")
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 from PhysicsTools.PatAlgos.tools.trackTools import *
 process.selectedLayer1TrackCands = cms.EDProducer("ConcreteChargedCandidateProducer",
@@ -102,88 +117,76 @@ src = cms.InputTag("goodTracks"),
 particleType = cms.string('mu+'),   # to fix mass hypothesis
 )
 ############################################
-## l1MuTkMatch - from l1muonsAnyBX and selectedLayer1TrackCands
-
+## -6- l1MuTkMatch - from l1muonsAnyBX and selectedLayer1TrackCands
 process.l1MuTkMatch = process.muonL1Info.clone()
 process.l1MuTkMatch.src     = cms.InputTag('selectedLayer1TrackCands') 
 process.l1MuTkMatch.matched = cms.InputTag('l1muonsAnyBX')
+process.l1MuTkMatch.resolveAmbiguitiesByMatchQuality = cms.bool(True)
+process.l1MuTkMatch.sortByRes = cms.string("deltaR")
 process.l1MuTkMatch.maxDeltaPhi = 0.3
 process.l1MuTkMatch.maxDeltaR   = 1
 process.l1MuTkMatch.maxDeltaEta = 0.4      
 process.l1MuTkMatch.writeExtraInfo = cms.bool(True)
-
 ############################################
-## trackMuonMatch from selectedLayer1TrackCands and l1MuTkMatch muToTkMatch
+## -8- trackMuonMatch from selectedLayer1TrackCands and l1MuTkMatch muToTkMatch
 from PhysicsTools.PatAlgos.producersLayer1.genericParticleProducer_cfi import patGenericParticles
 process.trackMuonMatch = patGenericParticles.clone(
     src = cms.InputTag("selectedLayer1TrackCands")
 )
 process.trackMuonMatch.userData.userCands.src += [cms.InputTag("l1MuTkMatch") ]
-#process.trackMuonMatch.userData.userFloats.src += [ cms.InputTag("l1MuTkMatch","quality")]
-#process.trackMuonMatch.userData.userFloats.src += [ cms.InputTag("l1MuTkMatch","RpcPtCode")]
+process.trackMuonMatch.userData.userCands.src += [cms.InputTag("muToTkMatch") ]
 ############################################
-## tagProbesJPsi from tagMuons and trackMuonMatch => JPsi
+## -9- tagProbesJPsi from tagMuons and trackMuonMatch => JPsi
 process.tagProbesJPsi = cms.EDProducer("CandViewShallowCloneCombiner",
 decay = cms.string("tagMuons@+ trackMuonMatch@-"), # charge coniugate states are implied; 'tagMuons' and 'trkProbes' should be collections of Candidates
 cut   = cms.string("2.8 < mass < 3.5"),                   
 )
 ############################################
-## tagProbesFilter from tagProbesMuTrk 
+## -10- tagProbesFilter from tagProbesMuTrk 
 process.tagProbesJPsiFilter = cms.EDFilter("PATCandViewCountFilter",
 minNumber = cms.uint32(1),
 maxNumber = cms.uint32(999999),
 src = cms.InputTag("tagProbesJPsi")
 )
 ############################################
-from MuonAnalysis.MuonAssociators.TnPTreeProducer_separation_variables_cfi import *
-process.tnpTreeJPsi = tnpTreeJPsiProducer.clone()
-treeProducer = process.tnpTreeJPsi
-from MuonAnalysis.TagAndProbe.nearbyMuonsInfo_cfi import nearbyMuonsInfo;
-tpp = treeProducer.tagProbePairs.moduleLabel
-setattr(process, tpp+"DiMuonInfo", nearbyMuonsInfo.clone(src = treeProducer.tagProbePairs))
-
-if not hasattr(treeProducer, 'pairVariables'):
-    treeProducer.pairVariables = cms.PSet()
-    treeProducer.pairFlags     = cms.PSet()
-treeProducer.pairVariables.dphiVtxTimesQ = cms.InputTag(tpp+"DiMuonInfo", "dphiVtxTimesQ")
-treeProducer.pairVariables.drM2          = cms.InputTag(tpp+"DiMuonInfo", "drM2")
-treeProducer.pairVariables.dphiM2        = cms.InputTag(tpp+"DiMuonInfo", "dphiM2")
-treeProducer.pairVariables.distM2        = cms.InputTag(tpp+"DiMuonInfo", "distM2")
-treeProducer.pairVariables.drStaIn       = cms.InputTag(tpp+"DiMuonInfo", "drStaIn")
-treeProducer.pairVariables.dphiStaIn     = cms.InputTag(tpp+"DiMuonInfo", "dphiStaIn")
-process.TFileService = cms.Service("TFileService", fileName = cms.string('TnPTree_JPsi.root'))
-process.tnpSequence = cms.Sequence(
-getattr(process, tpp+"DiMuonInfo")+
-process.tnpTreeJPsi
-)
-from MuonAnalysis.TagAndProbe.jpsi.tp_from_skim_common_cff import addDiMuonSeparationVariables
-addDiMuonSeparationVariables(process, process.tnpSequence, process.tnpTreeJPsi)
-
-
-
-############################################
 ########### P A T H ########################
 ############################################
 process.p = cms.Path(
-   process.preFilter *
-   process.l1muonsAnyBX *
+    process.preFilter +
+    process.l1muonsAnyBX *
    process.patMuonsWithTriggerSequence* 
    process.tagMuons *
    process.goodTracks *
    process.selectedLayer1TrackCands *
    process.l1MuTkMatch *
    process.trackMuonMatch *
-   process.tagProbesJPsi *
-   process.tagProbesJPsiFilter *
-   process.tnpSequence
+   process.tagProbesJPsi +
+   process.tagProbesJPsiFilter 
 )
-process.outpath = cms.EndPath()
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
-process.options   = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True),
+############################################
+########### O U T P U T ####################
+############################################
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('TnPPairs_data.root'),
+    outputCommands = cms.untracked.vstring('drop *',
+        'keep patMuons_patMuonsWithTrigger__*', 
+        'keep *_hltTriggerSummaryAOD_*_*',                   
+        'keep edmTriggerResults_TriggerResults_*_*',             
+        'keep l1extraL1MuonParticles_l1extraParticles_*_*',
+        'keep *_l1extraRPC_*_*',
+        'keep *_tagMuons_*_*',
+        'keep *_goodTracks_*_*',
+        'keep *_selectedLayer1TrackCands_*_*',
+        'keep *_l1muonsAnyBX_*_*',
+        'keep *_l1MuTkMatch_*_*',   
+        'keep *_trackMuonMatch_*_*',
+        'keep *_tagProbesJPsi_*_*',
+        'keep *_gtDigis_*_*',
+        'keep *_offlinePrimaryVertices__*',                   
+        'keep *_offlineBeamSpot__*',                        
+        'keep recoTrackExtras_standAloneMuons_*_*',
+        'keep *_*_*_RPCTagAndProbe'       
+    ),
+    SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('p') )
 )
-
- 
-print process.p
-
+process.e = cms.EndPath(process.out)
