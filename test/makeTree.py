528560f1af21f9f26e763f58d24dd302e926e63e
Marcin Konecki <marcin.konecki@cern.ch>
2015-08-29 15:03:46 +0200

    test

--- test/makeTree.py
+++ test/makeTree.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Analysis")
 import os
 
-#test2
+#test3
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 #
